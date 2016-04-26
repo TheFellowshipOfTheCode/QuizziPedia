67cf1fa79a687c891f8fa51a8b318f4675d3c9f1
Matteo Granzotto <granzotto.matteo@gmail.com>
2016-04-26 23:53:06 +0200

    Corretto file package - Modificato server in modo che mandi al front-end

--- Back-End/Config/Config.js
+++ Back-End/Config/Config.js
@@ -24,18 +24,27 @@ module.exports = function(app) {
     // set up our express application
     app.use(morgan('dev')); // log every request to the console
     app.use(cookieParser()); // read cookies (needed for auth)
-    app.use(bodyParser()); // get information from html forms
+    app.use(bodyParser.urlencoded());
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(express.static(path.join('../../', 'Front-End')));
     // required for passport
-    app.use(session({ secret: 'anything'})); // session secret
+    app.use(session({
+        secret: "cookie_secret",
+        resave: true,
+        saveUninitialized: true
+    })); // session secret
     app.use(flash()); // use connect-flash for flash messages stored in session
     app.use(passport.initialize());
     app.use(passport.session()); // persistent login sessions
     // routes ======================================================================
+    // Routing for the application
+     app.get('/*',function(req,res){
+        var path = require('path');
+        res.sendFile(path.resolve('Front-End/index.html'));
+     });
     require('../App/Routes/UserRouter.js')(app);
-    require('../App/Routes/QuizRouter.js')(app); 
-    require('../App/Routes/QuestionRouter.js')(app); 
-    require('../App/Routes/LangRouter.js')(app); 
+    require('../App/Routes/QuizRouter.js')(app);
+    require('../App/Routes/QuestionRouter.js')(app);
+    require('../App/Routes/LangRouter.js')(app);
 };

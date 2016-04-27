33959623eb5591bd47642c1521ed8235aad0023c
Matteo Granzotto <Blundert@users.noreply.github.com>
2016-04-27 18:42:23 +0200

    Corretto Config.js

--- Back-End/Config/Config.js
+++ Back-End/Config/Config.js
@@ -27,7 +27,7 @@ module.exports = function(app) {
     app.use(bodyParser.urlencoded());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(express.static(path.join('../../', 'Front-End')));
+    app.use(express.static(__dirname + '/../../Front-End'));
     // required for passport
     app.use(session({
         secret: "cookie_secret",

a28ddd8f441aba3c99cabfa73dc62b3268618585
Simone <simone.magagna@studenti.unipd.it>
2016-04-27 16:26:05 +0200

    Controller

---
+++ Front-End/Controllers/PasswordForgotController.js
@@ -0,0 +1,19 @@
+<!--
+/*******************************************************************************
+* Name: QuizziPedia::Front-End::Views::PasswordForgotController
+* Description:
+* Relations with other classes:
+* + PasswordForgoView
+* + AuthService
+* Creation data: 27-04-2016
+* Author: Simone Magagna
+********************************************************************************
+* Updates history
+*-------------------------------------------------------------------------------
+* ID: PasswordForgotController_20160427
+* Update data: 27-04-2016
+* Description: creazione
+* Author: Simone Magagna
+*-------------------------------------------------------------------------------
+*******************************************************************************/
+-->

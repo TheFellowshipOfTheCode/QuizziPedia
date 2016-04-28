a512a7a98289fe869f2804b18717f61bc1d9d834
mavarott <mavarott@gmail.com>
2016-04-28 16:23:01 +0200

    Definito ErrorModel e scritti i relativi test.

--- Back-End/App/Model/ErrorModel.js
+++ Back-End/App/Model/ErrorModel.js
@@ -1,39 +1,43 @@
 /*******************************************************************************
- * Name: Nome della classe da DDP; (QuizziPedia::ciao::prova::...)
- * Description: scrivere una piccola descrizione della classe (riassunto da DDP).
+ * Name: QuizziPedia::Back-End::App::Models::ErrorModel
+ * Description: classe che rappresenta le informazioni di un errore che si è
+ * verificato eseguendo una determianta operazione.
  * Relations with other classes:
- * + NomeAltraClasse1
- * + NomeAltraClasse2
+ * + IN ErrorHandler
  * Creation data: 27-04-2016
- * Author: Nome Cognome
+ * Author: Mattia Varotto
  ********************************************************************************
  * Updates history
  *-------------------------------------------------------------------------------
- * ID: NomeClasse_aaaammgg
- * Update data: gg-mm-aaaa
- * Description: descrizione della modifica fatta.
- * Autore: Nome Cognome
+ * ID: ErrorModel_20160427
+ * Update data: 28-04-2016
+ * Description: definito il modello e aggiunti i metodi getCode(), getTitle() e
+ * get Message().
+ * Autore: Mattia Varotto
  *-------------------------------------------------------------------------------
  *******************************************************************************/
+"use strict";
 let mongoose = require('mongoose');
 
 let errorSchema = new mongoose.Schema({
     errorCode: Number,
-    errorMessage: String,
-    errorTitle : String
+    errorTitle : String,
+    errorMessage: String
 });
 
 errorSchema.methods.getCode = function() {
     return this.errorCode;
 };
 
-errorSchema.methods.getMessage = function() {
-    return this.errorMessage;
-};
-
 errorSchema.methods.getTitle = function() {
     return this.errorTitle;
 };
 
-let Errors = mongoose.model('Errors', errorSchema);
-module.exports = Errors;
+errorSchema.methods.getMessage = function() {
+    return this.errorMessage;
+};
+
+// vedere se si riesce ad inserire gli errori nel database e fare i test relativi
+
+let ErrorModel = mongoose.model('Errors', errorSchema);
+module.exports = ErrorModel;

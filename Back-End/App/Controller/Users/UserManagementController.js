7f534917c75afc0b135f518824b18a725300abb5
marcomak91 <marcomak91@hotmail.it>
2016-04-29 16:30:41 +0200

    UserManagementController

--- Back-End/App/Controller/Users/UserManagementController.js
+++ Back-End/App/Controller/Users/UserManagementController.js
@@ -1,5 +1,7 @@
 
 var user = require('../../Model/UserModel');
+var summary = require('../../Model/SummaryModel');
+var quiz = require('../../Model/QuizModel');
 
 exports.updateDataUser = function(req, res, next) {
 
@@ -23,7 +25,7 @@ exports.deleteUser = function(req, res, next) {
 
 
 exports.getInfo = function(req, res, next) {
-    user.findOne({username:req.user.username},function(datauser){
+    user.findOne({'username':req.user.username},function(datauser){
         var info={
             username: datauser.username,
             name:datauser.name,
@@ -31,17 +33,35 @@ exports.getInfo = function(req, res, next) {
             email: datauser.email,
             userImg: datauser.userImg,
             experienceLevel: datauser.experienceLevel
-        }
-        res.send(info);
+        };
+        return res.send(info);
     })
 };
 
 exports.getSummary= function(req, res, next) {
-
+    summary.findOne({'_id':req.param('summaryId')}, function(dataSummary){
+        var summary={
+            quiz: dataSummary.quiz,
+            givenAnswers: dataSummary.givenAnswers,
+            data: dataSummary.data,
+            mark: dataSummary.mark
+        };
+        return res.send(summary);
+    })
 };
 
 exports.getSummaries = function(req, res, next) {
-
+    req.user.getSummaries(function(error,summaries){
+        if(err) return res.status(500).json(err.findOne({code:700}));
+        var query=summary.find({'quiz':{$in:summaries.quizSummaries.quiz}});
+        var query2=quiz.find({'_id':{$in:query.quiz}});
+        var dataSummaries={
+            id: quey2._id,
+            title: query2.title,
+            date: query.date
+        };
+        return res.send(dataSummaries);
+    });
 };
 
 exports.getUsers = function(req, res, next) {

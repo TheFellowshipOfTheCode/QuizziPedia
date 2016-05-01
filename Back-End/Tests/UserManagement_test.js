a6fdb3022eec91e537609708d38f5eca676657b5
FRANCO BERTON <franco.berton93@gmail.com>
2016-05-01 12:12:48 +0200

    UsermanagementTest

--- Back-End/Tests/UserManagement_test.js
+++ Back-End/Tests/UserManagement_test.js
@@ -3,26 +3,7 @@ var app = require('../Server.js');
 var request = require("supertest");
 var should = require("should")
 
-
-describe("signin", function() {
-    it("signin user", function (done) {
-        var userJSON = {username:'fberton', password:'ciaociao'}
-        request(app)
-            .post('/api/:lang/signin')
-            .send(userJSON)
-            .expect(200)
-            .expect('Content-Type', /json/ )
-            .end(function(err,res){
-                if (!err && res.status==200)
-                    if (res.body.success==false){
-                        res.body.message.should.equal("Login non effettuato")}
-                    else{
-                        res.body.user.username.should.equal("fberton")}
-            });
-        done()
-    });
-}))
-
+/*
 describe("getInfo", function() {
     it("check info user", function (done) {
         request(app)
@@ -39,4 +20,4 @@ describe("getInfo", function() {
             });
         done()
     });
-})
+})*/

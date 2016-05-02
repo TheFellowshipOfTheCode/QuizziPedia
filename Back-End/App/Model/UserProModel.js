b7a9f14549f52ca2fa766d3e0a54f17f9b75074a
francoberton <franco.berton93@gmail.com>
2016-05-02 13:06:48 +0200

    Authentication updated

--- Back-End/App/Model/UserProModel.js
+++ Back-End/App/Model/UserProModel.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var userProSchema = new mongoose.Schema(
     {
         userId: {
-            type: moongoose.Schema.Types.ObjectId,
+            type: mongoose.Schema.Types.ObjectId,
             ref: 'User'
         }
     }

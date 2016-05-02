db94fe79986c22f1a8e82d4fcd03fd912b2e6785
francoberton <franco.berton93@gmail.com>
2016-05-02 17:24:00 +0200

    Authentication updated

--- Back-End/App/Controller/Users/AuthenticationController.js
+++ Back-End/App/Controller/Users/AuthenticationController.js
@@ -51,7 +51,7 @@ exports.signup = function(req, res, next) {
             return next(err);
         if (!user)
         {   console.log(info);
-            return res.status(500).json({code:2, title:"no registration", message:'Registrazi'});}
+            return res.status(500).json({code:2, title:"no registration", message:'Registrazione effettuata'});}
         else
             return res.status(200).json({code:3, title:"ok registration", message:'Registrazione effettuata'})
     })(req, res, next);

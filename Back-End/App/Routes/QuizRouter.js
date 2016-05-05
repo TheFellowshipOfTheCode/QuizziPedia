326b40148f255574e9470a65172d2e0ee357f0d9
mavarott <mavarott@gmail.com>
2016-05-05 12:02:13 +0200

    Modifiche alle classi dei quiz #7

--- Back-End/App/Routes/QuizRouter.js
+++ Back-End/App/Routes/QuizRouter.js
@@ -2,9 +2,7 @@ var QuizController = require('../Controller/QuizController.js');
 
 module.exports = function(app){
     app.route('/api/:lang/user/quiz')
-        .post(QuizController.createQuiz);
-
-    app.route('/api/:lang/user/quiz')
+        .post(QuizController.createQuiz)
         .put(QuizController.editQuiz);
 
     app.route('/api/:lang/user/quiz/:quizId')

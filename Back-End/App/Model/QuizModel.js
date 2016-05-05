f8edc66096a3f239672395e5ed8127506ab48d95
mavarott <mavarott@gmail.com>
2016-05-05 11:15:04 +0200

    Modifiche alle classi dei quiz #6

--- Back-End/App/Model/QuizModel.js
+++ Back-End/App/Model/QuizModel.js
@@ -27,10 +27,9 @@ var quizSchema = new mongoose.Schema({
 
 
 quizSchema.statics.createQuiz = function(author, info, callback) {
-    var quiz = new this();
-    quiz.title = info.title;
-    quiz.author = author;
-    return quiz.save(callback);
+    info.author = author;
+    var new_quiz = new this(info);
+    return new_quiz.save(callback);
 }
 
 quizSchema.statics.editQuiz = function(info, callback) {

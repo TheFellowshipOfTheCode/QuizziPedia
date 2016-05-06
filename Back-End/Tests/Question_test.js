29b9a74bc3ad0ec3ad95d85521e4d4b56becf0dd
marcomak91 <marcomak91@hotmail.it>
2016-05-06 10:27:32 +0200

    ora ritorna anche makeWith e author

--- Back-End/Tests/Question_test.js
+++ Back-End/Tests/Question_test.js
@@ -63,7 +63,7 @@ describe("View Question Test", function(){
                 topic: "Patente",
                 keywords:["Strada","Guida"],
                 level:500,
-                alreadyAnswered:["5729c0fdc80eb653c3029c4e"]
+                //alreadyAnswered:["5729c0fdc80eb653c3029c4e"]
             })
             .end(function(err,res){
                 if (!err && res.status == 200){

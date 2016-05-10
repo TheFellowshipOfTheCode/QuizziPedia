63807dddaf91d7d099ba9d587699861b087a995d
Matteo Granzotto <granzotto.matteo@gmail.com>
2016-05-10 23:43:56 +0200

    /:lang/questions sistemato grafica

---
+++ Front-End/Directives/LinkingAnswerPreviewDirective.js
@@ -0,0 +1,27 @@
+/*******************************************************************************
+* Name: QuizziPedia::Front-End::Directives::;
+* Description: ;
+* Relations with other classes:
+* +
+* Creation data: 03-05-2016;
+* Author: Matteo Granzotto;
+* License: MIT.
+********************************************************************************
+* Updates history
+*-------------------------------------------------------------------------------
+* ID: _20160503;
+* Update data: 03-05-2016;
+* Description: Creata la direttiva;
+* Author: Matteo Granzotto.
+*-------------------------------------------------------------------------------
+*******************************************************************************/
+
+app.directive('linkingAnswerPreviewDirective', linkingAnswerPreviewDirective);
+
+function linkingAnswerPreviewDirective() {
+  var directive = {
+    restrict: 'E',
+    templateUrl: 'Directives/LinkingAnswerPreviewDirective.html'
+  };
+  return directive;
+};
--- Front-End/Directives/OneQuestionDirective.js
+++ Front-End/Directives/OneQuestionDirective.js
@@ -21,7 +21,48 @@ app.directive('oneQuestionDirective', oneQuestionDirective);
 function oneQuestionDirective() {
     var directive = {
         restrict: 'E',
-        templateUrl: 'Directives/OneQuestionDirective.html'
+        templateUrl: 'Directives/OneQuestionDirective.html',
+        link: oneQuestionDirectiveFunction
     };
     return directive;
 }
+
+oneQuestionDirectiveFunction.$inject = ['$scope'];
+
+function oneQuestionDirectiveFunction ($scope) {
+  $scope.currentPage = 0;
+  $scope.pageSize = 15;
+  $scope.isActive = true;
+
+  $scope.goOn = function () {
+    angular.element(".scrollable").scrollTop(0,0);
+    $scope.currentPage=$scope.currentPage+1;
+  }
+
+  $scope.goBack = function () {
+    angular.element(".scrollable").scrollTop(0,0);
+    $scope.currentPage=$scope.currentPage-1;
+  }
+
+  $scope.numberOfPages=function(numberOfQuestions){
+    var value;
+      if(numberOfQuestions%$scope.pageSize > 0) {
+        value = Math.floor(numberOfQuestions/$scope.pageSize)+1;
+      }
+      else {
+        value = Math.floor(numberOfQuestions/$scope.pageSize);
+      }
+      return value;
+  };
+
+  $scope.rightColor = function () {
+    $scope.isActive= !$scope.isActive;
+  }
+}
+
+app.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start;
+        return input.slice(start);
+    };
+});

7a6ad86d3e029c1da70e31e271188bf1275929bd
Matteo Granzotto <granzotto.matteo@gmail.com>
2016-05-14 23:55:31 +0200

    sistemato problema lingua

--- Front-End/Controllers/LangController.js
+++ Front-End/Controllers/LangController.js
@@ -22,8 +22,8 @@
 
 app.controller('LangController', LangController);
 
-LangController.$inject = ['$location', '$mdBottomSheet', 'LangService', '$rootScope'];
-function LangController ($location, $mdBottomSheet, LangService, $rootScope) {
+LangController.$inject = ['$location', '$mdBottomSheet', 'LangService', '$rootScope', '$scope'];
+function LangController ($location, $mdBottomSheet, LangService, $rootScope, $scope) {
 
   $scope.goToNewLang= goToNewLang;
   function goToNewLang(lang) {

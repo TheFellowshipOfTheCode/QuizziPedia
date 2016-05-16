ab9b5bdfb783072319d34f03251d7d60bae082fa
Simone <simone.magagna@studenti.unipd.it>
2016-05-16 14:09:17 +0200

    diario startform

--- Front-End/Filters/StartFrom.js
+++ Front-End/Filters/StartFrom.js
@@ -1,3 +1,21 @@
+<!--
+/*******************************************************************************
+* Name: QuizziPedia::Front-End::Filters::StartForm;
+* Description: classe che permette di restituire una sezione di un array;
+* Creation data: 03-05-2016;
+* Author: Matteo Granzotto;
+* License: MIT.
+********************************************************************************
+* Updates history
+*-------------------------------------------------------------------------------
+* ID: CreateQuestionnaireView_20160305;
+* Update data: 03-05-2016;
+* Description: Creata la classe;
+* Author: Matteo Granzotto.
+*-------------------------------------------------------------------------------
+*******************************************************************************/
+-->
+
 app.filter('startFrom', function() {
 
   return function(input, start) {

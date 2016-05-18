ebef3292a393343f2efe4c13dae8b6ccfcbaf490
francoberton <franco.berton93@gmail.com>
2016-05-18 13:02:03 +0200

    updated create question controller

--- Back-End/App/Controller/QuestionController.js
+++ Back-End/App/Controller/QuestionController.js
@@ -33,6 +33,7 @@ var Question = require('../Model/QuestionModel');
 var Topic= require('../Model/TopicModel');
 
 exports.createQuestion = function(req, res) {
+    console.log(req.body)
     Question.createQuestion(req.user._id, req.body, function(err, question){
         if(err) return res.status(500).json({code:88, title: "Errore Domanda", message: "Domanda non creata"});
         else {

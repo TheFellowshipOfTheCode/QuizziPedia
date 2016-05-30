253d45dde09935d4c4e2ab3ca87e56bbd13d2eb2
Alberto Ferrara <albertoferrara92@gmail.com>
2016-05-30 10:46:19 +0200

    update update images

--- Back-End/App/Controller/Users/UserManagementController.js
+++ Back-End/App/Controller/Users/UserManagementController.js
@@ -47,11 +47,16 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, '../Front-End/Images/Members'); // Le immagini verranno uploadate qui
+        fs.mkdirSync('prova');
+        callback(null, 'Front-End/Images/Members'); // Le immagini verranno uploadate qui
     },
     filename: function (req, file, callback) {
+<<<<<<< Updated upstream
         if (req.user.userImg != 'Images/Members/user-default.png')
             fs.unlink('../Front-End/'+req.user.userImg);
+=======
+        fs.unlink('Front-End/'+req.user.userImg);
+>>>>>>> Stashed changes
         callback(null, req.user._id + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]); // Vogliamo che l'immagine salvata mantenga il nome originale
     }
 });

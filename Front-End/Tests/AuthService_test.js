ebc8bb521167405d11242e647f6bcd78d7b448ae
Matteo Granzotto <granzotto.matteo@gmail.com>
2016-06-01 17:41:22 +0200

    Primo test funzionante ad un service

--- Front-End/Tests/AuthService_test.js
+++ Front-End/Tests/AuthService_test.js
@@ -1,26 +1,24 @@
 /*******************************************************************************
  * Name: AuthServiceTest;
- * Description: test di unità per la classe
- * QuizziPedia::Front-End::Controllers::AuthService;
- * Relations with other classes:
- * + .
+ * Description: test di unità per la classe;
+ * QuizziPedia::Front-End::Tests::AuthService_test;
  * Creation data: 26-05-2016;
- * Author: Alberto Ferrara;
+ * Author: Matteo Granzotto;
  * License: MIT.
  ********************************************************************************
  * Updates history
  *-------------------------------------------------------------------------------
- * ID: AuthServiceTest_20160526;
- * Update data: 01-05-2016;
+ * ID: AuthServiceTest_20160601;
+ * Update data: 01-06-2016;
  * Description: Scritto il test;
- * Author: Alberto Ferrara.
+ * Author: Matteo Granzotto.
  *-------------------------------------------------------------------------------
  *******************************************************************************/
-/*
+
  "use strict";
 
-var objRes = {
-  "data": {
+var objRes =
+  {
     "_id": "573b06bbade95afa018870e3",
     "privilege": "pro",
     "name": "Matteo",
@@ -40,245 +38,13 @@ var objRes = {
         "totalAnswers": 0,
         "correctAnswers": 0,
         "topicLevel": 500
-      },
-      {
-        "topicName": "Patente",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Letteratura Italiana",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Letteratura Straniera",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Filosofia",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Medicina",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Informatica UNIPD",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Biologia",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Cucina",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Cinema",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Economia e Finanza",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Tecnologia",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Fisica",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Elettronica",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Astronomia",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Arte",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Geologia",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Moda",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Politica",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "TV e Spettacolo",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Caccia e Pesca",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Logica",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Motori",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Giurisprudenza e Diritto",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Chimica",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Geometria",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Geografia",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Videogiochi",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Fumetti e Manga",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Animali",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Informatica",
-        "totalAnswers": 2,
-        "correctAnswers": 2,
-        "topicLevel": 521
-      },
-      {
-        "topicName": "Sport",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Musica",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Architettura",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Matematica",
-        "totalAnswers": 1,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Psicologia",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
-      },
-      {
-        "topicName": "Storia",
-        "totalAnswers": 0,
-        "correctAnswers": 0,
-        "topicLevel": 500
       }
     ],
     "userImg": "Images/Members/573b06bbade95afa018870e3.jpg"
-  },
-  "status": 200,
-  "config": {
-    "method": "GET",
-    "transformRequest": [
-      null
-    ],
-    "transformResponse": [
-      null
-    ],
-    "url": "/api/it/loggedin",
-    "headers": {
-      "Accept": "application/json, text/plain, "
-    }
-  },
-  "statusText": "OK"
-};
+  };
 
 
-describe("reddit api service", function () {
+describe("AuthService api rest unit test", function () {
   var redditService, httpBackend;
 
 
@@ -296,22 +62,18 @@ describe("reddit api service", function () {
   });
 
 
-  it("should do something", function () {
-    console.log("arrivo qui");
+  it("should give back to the server the authticathed user", function () {
     httpBackend.expectGET("/api/it/loggedin");
     httpBackend.whenGET(/Views/).respond(200, '');
-    console.log("ho risposto");
     redditService.giveMe("it").then(function(subreddits) {
-      console.log("Entro qui");
-      console.log(subreddits);
-      expect(JSON.stringify(subreddits)).toEqual(
+      expect(JSON.stringify(subreddits.data)).toEqual(
         JSON.stringify(objRes)
       );
     }, function(err) {
-      console.log(err);
-    }
+        console.log(err);
+      }
     );
     httpBackend.flush();
   });
 
-});*/
+});

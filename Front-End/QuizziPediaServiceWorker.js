02e52acfbebe21792568a9276bc5badb637a2c6d
Matteo Granzotto <granzotto.matteo@gmail.com>
2016-06-08 16:09:17 +0200

    Iniziata pagina di errore

--- Front-End/QuizziPediaServiceWorker.js
+++ Front-End/QuizziPediaServiceWorker.js
@@ -1,45 +1,7 @@
-
 importScripts('/ServiceWorker/cache-polyfill.js');
-
-/*console.log('Started', self);
-
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-  console.log('Installed', e);
-  e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/ServiceWorker/Views/OfflineIndex.html',
-       '/ServiceWorker/Views/OfflineIndex.html?homescreen=1',
-       '/?homescreen=1',
-       '/ServiceWorker/css/main-offline.css',
-       '/ServiceWorker/Images/Icons/logo.png'
-
-     ]);
-   })
- );
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('Activated', event);
-});
-self.addEventListener('push', function(event) {
-  console.log('Push message received', event);
-  // TODO
-});
-
-self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
- event.respondWith(
-   caches.match(event.request).then(function(response) {
-       return response || fetch(event.request);
-     })
- );
-});*/
 
 self.addEventListener('install', function(event) {
   // Put `offline.html` page into cache
-  console.log('Installed', event);
 
   var offlineRequest = new Request('/ServiceWorker/Views/OfflineIndex.html');
   event.waitUntil(
